7b5f53d94e2be7a96ea3955eca5dfceb033bad26
Mario Maric <mario09kpd@gmail.com>
2026-06-12 00:39:50 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,15 +1,19 @@
- const mysql = require("mysql2");
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "budget_app"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
-    if (err) console.log(err);
-    else console.log("Povezan sa bazom");
+    if (err) {
+        console.error("Greška pri konekciji:", err);
+    } else {
+        console.log("Povezan na Railway MySQL!");
+    }
 });
 
 module.exports = db;
